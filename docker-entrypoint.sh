083285500415e2cf3e67346f5eb8aec81fa0760e
Philip Garnero <philip.garnero@gmail.com>
2016-07-27 03:33:47 +0200

    django backend with react frontend

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,6 @@ if [ "$1" = 'start' ]; then
     cd /app
     python manage.py createcachetable
     python manage.py migrate
-    python manage.py compress
-    python manage.py collectstatic --noinput
     exec gunicorn -w ${NUM_WORKERS:-2} -t ${TIMEOUT:-120} \
                   --log-level=info \
                   -b 0.0.0.0:8000 \
